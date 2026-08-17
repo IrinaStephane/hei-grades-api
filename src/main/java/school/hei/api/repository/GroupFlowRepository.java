c7231401e09a373826d0d4bf91d1c31519a9ff77
Irina <irinarakoto2005@gmail.com>
2026-08-17 13:54:29 +0300

    feat: add repositories for grades domain entities

---
+++ src/main/java/school/hei/api/repository/GroupFlowRepository.java
@@ -0,0 +1,15 @@
+package school.hei.api.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import school.hei.api.model.GroupFlow;
+
+public interface GroupFlowRepository extends JpaRepository<GroupFlow, String> {
+
+  List<GroupFlow> findByStudentIdOrderByFlowDatetimeAsc(String studentId);
+
+  List<GroupFlow> findByGroupIdOrderByFlowDatetimeAsc(String groupId);
+
+  Optional<GroupFlow> findFirstByStudentIdOrderByFlowDatetimeDesc(String studentId);
+}
