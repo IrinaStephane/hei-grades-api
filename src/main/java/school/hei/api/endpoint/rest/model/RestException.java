4a1198f866e0e636e5e7dcd5f238ab0786c4c5f1
Mena845 <tsikiniainatsilavina@gmail.com>
2026-08-18 10:14:32 +0300

    feat: refactor RestException to use Lombok annotations for improved readability and maintainability

--- src/main/java/school/hei/api/endpoint/rest/model/RestException.java
+++ src/main/java/school/hei/api/endpoint/rest/model/RestException.java
@@ -1,3 +1,17 @@
 package school.hei.api.endpoint.rest.model;
 
-public record RestException(String type, String message) {}
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RestException {
+    private String type;
+    private String message;
+}
